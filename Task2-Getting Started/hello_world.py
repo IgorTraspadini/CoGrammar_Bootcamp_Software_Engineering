0cb62fd980b3ae8a736d30420342b44693a1ba31
IgorTraspadini <igorchieppe@hotmail.com>
2024-12-17 06:51:39 -0300

    Task2-1

---
+++ Task2-Getting Started/hello_world.py
@@ -0,0 +1,21 @@
+# Created by IgorCT - Dez_2024
+
+# pseudocode:
+# 1. take a user name input using input() function
+# 2. print out the name using print() function
+# 3. take the user age using input() function
+# 4. print out the age using print() function
+# 5. print out the string "Hello World" on a new line (default)
+
+# get user name and store in variable user_name
+user_name = input("Please, enter your name: ")
+# print user name
+print(user_name)
+
+# get user age and store in variable user_age
+user_age = str(input("Please, enter your age: "))
+# print user name
+print(user_age)
+
+# print out the string "Hello World"
+print("Hello World")  # default print on a new line
