7dc8947e4962ee993814003ae6dde45fea9db74d
IgorTraspadini <igorchieppe@hotmail.com>
2025-01-06 13:34:13 +0000

    Task4 adjustment

--- pattern.py
+++ pattern.py
@@ -1,11 +1,16 @@
-# Created by IgorCT - Dez_2024
-
-# variable
+# Variable to keep track of the number of rows for the reverse pattern
 count_rev = 5
 
-for i in range(1,10):
-  if i < 6:
-    print(i*"*")
-  else:
-    count_rev -= 1
-    print(count_rev*"*")
+# Loop through each row from 1 to 9
+for i in range(1, 10):
+    # Check if the current row is less than 6
+    if i < 6:
+        # Print a pattern of asterisks (*) for the current row
+        # The number of asterisks increases by 1 for each row
+        print(i * "*")
+    else:
+        # Decrement the count for the reverse pattern
+        count_rev -= 1
+        # Print a pattern of asterisks (*) for the current row
+        # The number of asterisks decreases by 1 for each row
+        print(count_rev * "*")
--- while.py
+++ while.py
@@ -1,13 +1,18 @@
 # Created by IgorCT - Dez_2024
 
-# variables
+# Variables to store user input and a list of numbers
 user_input = 0
-average_list = []
+number_list = []
 
-# while loop 
+# While loop to continuously ask for user input until -1 is entered
 while user_input != -1:
-  # user input
-  user_input = int(input("Enter a number different of zero (-1 to exit): "))
-  if user_input != 0:
-    average_list.append(user_input)
-    print(f"Numbers average: {sum(average_list)/len(average_list)}")
+    # Ask the user to enter a number different from zero (-1 to exit)
+    user_input = int(input("Enter a number different from zero (-1 to exit): "))
+
+    # Check if the user input is not zero and not -1
+    if user_input != 0 and user_input != -1:
+        # Append the user input to the list of numbers
+        number_list.append(user_input)
+
+# Calculate and print the average of the numbers in the list
+print(f"Numbers average: {sum(number_list)/len(number_list)}")
